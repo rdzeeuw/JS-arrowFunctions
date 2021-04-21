950f98d49642941c385de97332cacc4d1806f90e
rdzeeuw <77979140+rdzeeuw@users.noreply.github.com>
2021-04-21 10:09:11 +0200

    Add files via upload

---
+++ JS Arrow Functions/script.js
@@ -0,0 +1,14 @@
+//Deel A
+const ikRockArrowFunctions = () => {console.log("Joe, ik rock de arrow functions!")};
+
+//Deel B
+const fivePlusSeven = () => 5 + 7;
+
+//Deel C
+let myFunction = (a,b) => a + b;
+
+//Deel D
+let addFive = a => a + 5;
+
+//Deel E
+const createObject = () => ({greeting: hoi});
